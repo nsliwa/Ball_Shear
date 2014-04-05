f7f199bfe8aaae849097c3ad4502ac9b7318bb1d
georgejoel <georgejoel@gmail.com>
2014-04-05 12:22:29 -0500

    fixed errors for live feed

--- imagedetection/livefeed.py
+++ imagedetection/livefeed.py
@@ -1,5 +1,4 @@
-import cv2
-
+import cv2,sys
 cv2.namedWindow("Live Feed")
 vc = cv2.VideoCapture(2)
 
@@ -10,35 +9,58 @@ else:
 
 # Captures a single image from the camera and returns it in PIL format
 def get_image():
- # read is the easiest way to get a full image out of a VideoCapture object.
- retval, im = vc.read()
- return im
+    # read is the easiest way to get a full image out of a VideoCapture object.
+    retval, im = vc.read()
+    return im
 
 def capture():
-	camera_capture = get_image()
-	file = "test_image.png"
-	cv2.imwrite(file, camera_capture)
-	display()
+    camera_capture = get_image()
+    print(len(sys.argv))
+    if(len(sys.argv)==1):
+        file="capturedimage.png"
+    else:
+        file = sys.argv[1]+"/capturedimage.png"
+    cv2.imwrite(file, camera_capture)
+    a =display()
+    if(a == 2):return 2
 
 def display():
-	print("Press ESC to Delete")
-	print("Press S to Save")
-	imgFile = cv2.imread('test_image.png')
-	cv2.imshow('image', imgFile)
-	key = cv2.waitKey(0)
-	if key == 27: cv2.destroyWindow('image')	
-	if key == 115:cv2.destroyAllWindow()
+    print("Press ESC to Delete")
+    print("Press S to Save")
+    if(len(sys.argv)==1):
+        imgFile=cv2.imread('capturedimage.png')
+        cv2.imshow('image', imgFile)
+    else:
+        imgFile = cv2.imread(sys.argv[1]+'/capturedimage.png')
+        cv2.imshow('image', imgFile)
+    key = cv2.waitKey(0)
+    if key == 27:
+        cv2.destroyWindow('image')
+        return 1
+    if key == 115:
+        cv2.destroyWindow('image')
+        cv2.destroyWindow("Live Feed")
+        return 2
+        
+	
 
 print("Press c to capture a Image")
+#print(sys.argv[1])
 while rval:
     cv2.imshow("Live Feed", frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
-
+    b=0
     #key2 = cv2.waitKey(0);
     if key == 27: # exit on ESC
         break
     if key == 99:
-    	capture()
+    	b =capture()
+    if (b==2):
+        break
+    	
+    	
+    
 		
-cv2.destroyWindow("preview")
+cv2.destroyWindow("Live Feed")
+
